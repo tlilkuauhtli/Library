e3ff80a8916030f254877d7ddd6b200d49f560c6
tlilkuauhtli <tliltik_kuauhtli@hotmail.com>
2015-08-10 08:44:05 -0500

    Done

---
+++ DataAccess/DataAccess.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Library.Entities;
+
+namespace DataAccess
+{
+    public abstract class DataAccess : IDisposable
+    {
+        #region Enumerators
+        /// <summary>
+        /// Name of Connections
+        /// </summary>
+        public enum Connections
+        {
+            SqlDataAccess
+        }
+        #endregion
+
+        #region Variables
+        /// <summary>
+        /// Connection String
+        /// </summary>
+        protected string _connectionString;
+        /// <summary>
+        /// Log File Path
+        /// </summary>
+        protected string _logFilePath;
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Return the Connection String
+        /// </summary>
+        public String ConnectionString { get { return _connectionString; } }
+        #endregion
+
+        #region Constructor
+
+        protected DataAccess()
+        {
+            _connectionString = string.Empty;
+            //_logFilePath = AppDomain.CurrentDomain.BaseDirectory + CSUtilities.GetAppValue("SQLErrorLogFile") + DateTime.Now.ToString("yyyyMMdd") + ".txt";
+            _logFilePath = Utilities.GetParentPath(Utilities.GetAppValue("SQLErrorLogFolder")) + "\\"
+                + Utilities.GetAppValue("SQLErrorLogFile") + DateTime.Now.ToString("yyyyMMdd") + ".txt";
+        }
+
+        public abstract void Dispose();
+
+        #endregion
+
+        #region Methods
+
+        #region Internal
+        /// <summary>
+        /// Get a Specific Connection String
+        /// </summary>
+        /// <param name="connection">Name of the Connection String</param>
+        /// <returns>Connection String</returns>
+        internal string GetConnectionString(Connections connection)
+        {
+            try
+            {
+                return Utilities.GetConnectionString(connection.ToString());
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// Save exceptions into Log File
+        /// </summary>
+        /// <param name="text">Text to save</param>
+        public void SaveInLog(string text)
+        {
+            try
+            {
+                Utilities.WriteInFile(text, _logFilePath);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
---
+++ DataAccess/SQLDataAccess.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DataAccess
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class SQLDataAccess : DataAccess
+    {
+        #region Variables
+        private SqlConnection _connection;
+        #endregion
+    
+        #region Contructor
+        public SQLDataAccess()
+        {
+            _connectionString = GetConnectionString(Connections.SqlDataAccess);
+        }
+        #endregion
+
+        #region Methods
+
+        #region Public
+        public override void Dispose()
+        {
+            if (_connection != null)
+            {
+                if (_connection.State != ConnectionState.Closed) _connection.Close();
+                _connection.Dispose();
+            }
+        }
+        #endregion
+
+        #region Protected
+        /// <summary>
+        /// Execute the Command
+        /// </summary>
+        /// <param name="cmd">Command to Execute</param>
+        /// <returns>DataTable</returns>
+        protected DataTable ExecuteGetData(SqlCommand cmd)
+        {
+            try
+            {
+                var dataTable = new DataTable();
+                cmd.Connection.Open();
+                var dataAdapter = new SqlDataAdapter(cmd);
+                dataAdapter.Fill(dataTable);
+                return dataTable;
+            }
+            catch (Exception e)
+            {
+                SaveInLog(e.Message);
+                return null;
+            }
+        }
+
+        protected SqlConnection GetConnection()
+        {
+            return _connection ?? (_connection = new SqlConnection(_connectionString));
+        }
+        #endregion
+
+        #region Private
+        /// <summary>
+        /// Close the connecction
+        /// </summary>
+        private void CloseConnection()
+        {
+            if (_connection == null) return;
+            if (_connection.State != ConnectionState.Closed) _connection.Close();
+        }
+        #endregion
+
+        #endregion
+    }
+}
