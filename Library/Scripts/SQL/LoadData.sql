3669c589a321d3596c4e4679a4161b0467919d3e
tlilkuauhtli <tliltik_kuauhtli@hotmail.com>
2015-08-10 18:20:20 -0500

    Adding SQL Scripts to main Project

---
+++ Library/Scripts/SQL/LoadData.sql
@@ -0,0 +1,28 @@
+Use Library
+Go
+
+Insert Into Categories(Category_Id, Name) Values(1, 'Fantasy')
+Insert Into Categories(Category_Id, Name) Values(2, 'Horror')
+Insert Into Categories(Category_Id, Name) Values(3, 'History Novel')
+Insert Into Categories(Category_Id, Name) Values(4, 'Suspense')
+
+Insert Into Authors(Author_Id, First_Name, Surname) Values(1, 'Gary', 'Jennings')
+Insert Into Authors(Author_Id, First_Name, Surname) Values(2, 'Stephen', 'King')
+Insert Into Authors(Author_Id, First_Name, Surname) Values(3, 'Antonio', 'Guadarrama Collado')
+Insert Into Authors(Author_Id, First_Name, Surname) Values(4, 'Philip', 'Pullman')
+
+Insert Into Books(Book_Id, Name, Author_Id, Category_Id) Values(1, 'Aztec', 1, 3);
+Insert Into Books(Book_Id, Name, Author_Id, Category_Id) Values(2, 'Northern Lights', 4, 1);
+Insert Into Books(Book_Id, Name, Author_Id, Category_Id) Values(3, 'It', 2, 2);
+Insert Into Books(Book_Id, Name, Author_Id, Category_Id) Values(4, 'Cuauhtli', 3, 3);
+Insert Into Books(Book_Id, Name, Author_Id, Category_Id) Values(5, 'Balam', 3, 3);
+Insert Into Books(Book_Id, Name, Author_Id, Category_Id) Values(6, 'Aztec Autom', 3, 3);
+Insert Into Books(Book_Id, Name, Author_Id, Category_Id) Values(7, 'Aztec Blood', 3, 3);
+Insert Into Books(Book_Id, Name, Author_Id, Category_Id) Values(8, 'Aztec Rage', 3, 3);
+
+Insert Into Books(Book_Id, Name, Author_Id, Category_Id) Values(9, 'The Subtle Knife', 4, 1);
+Insert Into Books(Book_Id, Name, Author_Id, Category_Id) Values(10, 'The Amber Spyglass', 4, 1);
+Insert Into Books(Book_Id, Name, Author_Id, Category_Id) Values(11, 'Coatl', 3, 3);
+Insert Into Books(Book_Id, Name, Author_Id, Category_Id) Values(12, 'Carrie', 2, 2);
+Insert Into Books(Book_Id, Name, Author_Id, Category_Id) Values(13, 'The Dead Zone', 2, 2);
+Insert Into Books(Book_Id, Name, Author_Id, Category_Id) Values(14, 'The Eyes of the Dragon', 2, 2);
